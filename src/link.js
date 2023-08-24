6f98595515c4fa1e447b6f8fb209c0fbd480372d
ibrahim hassan <103922175+Hassanibrahim0617@users.noreply.github.com>
2023-08-24 12:59:37 +0100

    branch

--- src/link.js
+++ src/link.js
@@ -29,64 +29,121 @@ export const products = [
         id: 1,
         image: '../images_folder/syltherine.svg',
         title: 'syltherine',
-        body:'Stylish cafe chair',
-        price:'Rp 3.500.000',
-        discount:'Rp 2.500.000'
+        body: 'Stylish cafe chair',
+        price: 'Rp 3.500.000',
+        discount: 'Rp 2.500.000'
     },
     {
         id: 2,
         image: '../images_folder/leviosa.svg',
         title: 'leviosa',
-        body:'Stylish cafe chair',
-        price:'',
-        discount:'Rp 2.500.000'
+        body: 'Stylish cafe chair',
+        price: '',
+        discount: 'Rp 2.500.000'
     },
     {
         id: 3,
         image: '../images_folder/Lolito.svg',
         title: 'Lolito',
-        body:'Luxury big sofa',
-        price:'Rp 14.000.000',
-        discount:'Rp 7.000.000'
+        body: 'Luxury big sofa',
+        price: 'Rp 14.000.000',
+        discount: 'Rp 7.000.000'
     },
     {
         id: 4,
         image: '../images_folder/Respira.svg',
         title: 'sRespira',
-        body:'SOutdoor bar table and stool',
-        price:'',
-        discount:'Rp 500.000'
+        body: 'SOutdoor bar table and stool',
+        price: '',
+        discount: 'Rp 500.000'
     },
     {
         id: 5,
         image: '../images_folder/Grifo.svg',
         title: 'Grifo',
-        body:'SNight lamp',
-        price:'',
-        discount:'Rp 1.500.000'
+        body: 'SNight lamp',
+        price: '',
+        discount: 'Rp 1.500.000'
     },
     {
         id: 6,
         image: '../images_folder/Muggo.svg',
         title: 'Muggo',
-        body:'Small mug',
-        price:'',
-        discount:'Rp 150.000'
+        body: 'Small mug',
+        price: '',
+        discount: 'Rp 150.000'
     },
     {
         id: 7,
         image: '../images_folder/Pingky.svg',
         title: 'Pingky',
-        body:'Cute bed set',
-        price:'Rp 14.000.000',
-        discount:'Rp 7.000.000'
+        body: 'Cute bed set',
+        price: 'Rp 14.000.000',
+        discount: 'Rp 7.000.000'
     },
     {
         id: 8,
         image: '../images_folder/Potty.svg',
         title: 'Potty',
-        body:'Minimalist flower pot',
-        price:'',
-        discount:'Rp 500.000'
+        body: 'Minimalist flower pot',
+        price: '',
+        discount: 'Rp 500.000'
+    },
+]
+
+// BEAUTIFUL ROOMS
+export const roomsDecor = [
+    {
+        id:1,
+        image:'https://imgur.com/zvENa5v'
+    },
+    {
+        id:2,
+        image:'https://imgur.com/zXe5sHy'
+    },
+    {
+        id:3,
+        image:'https://i.imgur.com/R9AfX5J.png'
+    },
+    
+]
+
+// FUNIRO FURNITURE
+export const furniture = [
+    {
+        id:1,
+        image:'https://imgur.com/PABbaLh'
+    },
+    {
+        id:2,
+        image:'https://imgur.com/Wcb5s77'
+    },
+    {
+        id:3,
+        image:'https://imgur.com/Ec3XhOS'
+    },
+    {
+        id:4,
+        image:'https://imgur.com/RjOVMJw'
+    },
+    {
+        id:5,
+        image:'https://imgur.com/Q8YomzV'
+    },
+    {
+        id:6,
+        image:'https://imgur.com/JnfDLFf'
+    },
+    {
+        id:7,
+        image:'https://imgur.com/oyxQXbj'
+    },
+    {
+        id:8,
+        image:'https://imgur.com/V4OhGem'
+    },
+    {
+        id:9,
+        image:'https://imgur.com/jL8zms9'
     },
 ]
