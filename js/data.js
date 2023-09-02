d0cb6dc4fdbfb49238109a254ee8c6f19773e733
Ibrahim Hassan <hassan.ibb120044@gmail.com>
2023-09-02 12:41:22 +0100

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -3,17 +3,54 @@ const footerGroup24 = document.querySelector('.group-24');
 const footerGroup25 = document.querySelector('.group-25');
 const formPayment = document.querySelectorAll('form');
 const countryRegion = document.querySelector(".rectangle-539")
+const firstname = document.querySelector('.rectangle-53');
+const lastname = document.querySelector('.rectangle-532');
+const companyName = document.querySelector('.rectangle-54');
+const streetAddress = document.querySelector('.rectangle-533');
+const zipCode = document.querySelector('.rectangle-534');
+const phoneNumber = document.querySelector('.rectangle-536');
+const emailAddress = document.querySelector('.rectangle-537');
+const town = document.querySelector('.rectangle-535');
+const bankTransfer = document.querySelector('#ellipse-12');
+const directBank = document.querySelector('#ellipse-13');
+const displayError = ''
+footerForm = document.querySelector('.footre-form')
 
 formPayment.forEach(forms => {
     forms.addEventListener('submit', async (e) => {
         try {
             e.preventDefault()
-localStorage.setItem('user', JSON.stringify(formPayment))
-            
-            // for (var i = 0; i < countries.length; i++) {
-            //     // countries[i].image = 'https://www.countryflags.io/' + countries[i].value.toLowerCase() + '/shiny/24.png';
-            // }
-            // console.log(countries)
+            if (
+                firstname.value.trim() === '' ||
+                lastname.value.trim() === '' ||
+                companyName.value.trim() === '' ||
+                streetAddress - address.value.trim() === '' ||
+                zipCode.value.trim() === '' ||
+                town.value.trim() === '' ||
+                phoneNumber.value.trim() === '' ||
+                emailAddress.value.trim() === '' ||
+                countryRegion.value.trim() === '' ||
+                bankTransfer.value === '' ||
+                directBank.value === ''
+            ) {
+                displayError.textContent = 'fill this space';
+                console.log(forms)
+            } else {
+                displayError.textContent = 'click on the place order';
+            }
+          
+            let formData = new FormData(forms);
+            let formDataObj = Object.fromEntries(formData);
+            localStorage.setItem("input", JSON.stringify(formDataObj));
+
+//             footerForm.addEventListener('submit',(event)=>{
+// event.preventDefault()
+// console.log(footerForm)
+// let footerFormData = new FormData(footerForm);
+//             let formDataObj = Object.fromEntries(footerFormData);
+//             localStorage.setItem("input", JSON.stringify(formDataObj));
+// })
+
         } catch (error) {
             console.log(error)
         }
@@ -25,10 +62,15 @@ const country = async () => {
     try {
         const response = await fetch(countriesName)
         const country = await response.json()
-
-        let inputCountry = ''
-        inputCountry += `
+        countries = ''
+        countries += `
+         for (var i = 0; i < country.length; i++) {
+            country[i].image = 'https://www.countryflags.io/' + country[i].value.toLowerCase() + '/shiny/24.png';
+        }
         `
+
+        countryRegion.innerHTML = countries
+        console.log(countries)
 
     } catch (error) {
         console.log(error)
@@ -51,13 +93,15 @@ const postlists = async () => {
         <li class="links"><a href="${source}?id=${id}">${title}</a></li>
         `
             footerGroup24.innerHTML = footerLi;
-            console.log(footer1)
+
         });
     } catch (error) {
         console.log(error)
     }
 
 }
+
+
 
 // FOOTER LI 2
 let footer2 = `http://localhost:3000/footerLi2`;
@@ -67,7 +111,7 @@ const postlists2 = async () => {
     try {
         const response = await fetch(footer2)
         const postlists2 = await response.json()
-        console.log(postlists2)
+
         let footerList = ``
         postlists2.forEach((list2) => {
             let { id, title, source } = list2;
@@ -83,6 +127,7 @@ const postlists2 = async () => {
     }
 
 }
+
 
 
 window.addEventListener('DOMContentLoaded', async () => postlists())
